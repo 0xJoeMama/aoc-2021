c978a0e3c61773487b9dd7cdaeaf3762676463f2
0xJoeMama <0xjoemama@gmail.com>
2021-12-24 20:33:57 +0200

    Reformat

--- Day-2/Problem.md
+++ Day-2/Problem.md
@@ -1,4 +1,5 @@
 # [Day 2](https://adventofcode.com/2021/day/2)
+
 Now, you need to figure out how to pilot this thing.
 
 It seems like the submarine can take a series of commands like forward 1, down 2, or up 3:
@@ -7,16 +8,13 @@ It seems like the submarine can take a series of commands like forward 1, down 2
     down X increases the depth by X units.
     up X decreases the depth by X units.
 
-Note that since you're on a submarine, down and up affect your depth, and so they have the opposite result of what you might expect.
+Note that since you're on a submarine, down and up affect your depth, and so they have the opposite result of what you
+might expect.
 
-The submarine seems to already have a planned course (your puzzle input). You should probably figure out where it's going. For example:
+The submarine seems to already have a planned course (your puzzle input). You should probably figure out where it's
+going. For example:
 
-forward 5
-down 5
-forward 8
-up 3
-down 8
-forward 2
+forward 5 down 5 forward 8 up 3 down 8 forward 2
 
 Your horizontal position and depth both start at 0. The steps above would then modify them as follows:
 
@@ -27,6 +25,8 @@ Your horizontal position and depth both start at 0. The steps above would then m
     down 8 adds 8 to your depth, resulting in a value of 10.
     forward 2 adds 2 to your horizontal position, a total of 15.
 
-After following these instructions, you would have a horizontal position of 15 and a depth of 10. (Multiplying these together produces 150.)
+After following these instructions, you would have a horizontal position of 15 and a depth of 10. (Multiplying these
+together produces 150.)
 
-Calculate the horizontal position and depth you would have after following the planned course. What do you get if you multiply your final horizontal position by your final depth?
+Calculate the horizontal position and depth you would have after following the planned course. What do you get if you
+multiply your final horizontal position by your final depth?
---
+++ setup/Setup.ts
@@ -0,0 +1,71 @@
+import {parseInput} from "../util/Parser.ts";
+
+const key = (await parseInput('./key.cfg'))
+    .split(/\n/)
+    .filter(it => !it.startsWith('#'))
+    .find(it => it.startsWith('session='));
+
+if (key === undefined) {
+    throw new Error('Could not parse `key.cfg` file or could not locate `session=` value!!')
+}
+
+export class Day {
+    private readonly index: number;
+
+    constructor(index: number) {
+        this.index = index;
+    }
+
+    async create(key: string) {
+        const dayPath = `${Deno.cwd()}/../Day-${this.index}`;
+        await Day.createLocallyAccessibleFiles(dayPath);
+        const encoder = new TextEncoder();
+
+        await this.requestProblemDescription(key, encoder, dayPath);
+        await this.requestProblemInput(key, encoder, dayPath);
+    }
+
+    private async requestProblemInput(key: string, encoder: TextEncoder, dayPath: string) {
+        const inputResponse: Response = await fetch(`https://adventofcode.com/2021/day/${this.index}/input`, {
+            credentials: 'include',
+            method: 'GET',
+            headers: {
+                Cookie: key
+            }
+        });
+
+        const inputData: Uint8Array = encoder.encode(await inputResponse.text());
+        await Deno.writeFile(`${dayPath}/input.txt`, inputData);
+    }
+
+    private async requestProblemDescription(key: string, encoder: TextEncoder, dayPath: string) {
+        const problemResponse: Response = await fetch(`https://adventofcode.com/2021/day/${this.index}`, {
+            credentials: 'include',
+            method: 'GET',
+            headers: {
+                Cookie: key
+            }
+        });
+        const dataBuffer: Uint8Array = encoder.encode(await problemResponse.text());
+        await Deno.writeFile(`${dayPath}/Problem.html`, dataBuffer);
+    }
+
+    private static async createLocallyAccessibleFiles(dayPath: string) {
+        await Deno.mkdir(dayPath);
+        await Deno.copyFile(`${Deno.cwd()}/Day-n/Solution.ts.template`, `${dayPath}/Solution.ts`);
+        await Deno.copyFile(`${Deno.cwd()}/Day-n/run.sh`, `${dayPath}/run.sh`);
+    }
+}
+
+const utilMain = async () => {
+    let day = Number.parseInt(Deno.args[0]);
+
+    if (!day) {
+        day = Number.parseInt(prompt('What day would you like to setup? ') ?? "");
+    }
+
+    const dayInstance = new Day(day);
+    await dayInstance.create(key);
+};
+
+await utilMain();
