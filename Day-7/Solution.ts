f52f9d3a8cc758a5bd981648d52c181748633518
0xJoeMama <0xjoemama@gmail.com>
2021-12-25 19:16:03 +0200

    Day 7 2.0

---
+++ Day-7/Solution.ts
@@ -0,0 +1,46 @@
+import {parseInput} from "../util/Parser.ts";
+
+const input: string[] = (await parseInput(Deno.args[0])).split(/\n/).filter(it => it !== '');
+
+type UsageMap = {
+    [pos: number]: number;
+};
+
+type FuelFormula = (pos: number, crabPos: number) => number;
+
+function calculateFuel(pos: number, crabPoses: number[], fuelCalc: FuelFormula): number {
+    return crabPoses.map(it => fuelCalc(pos, it)).reduce((acc, val) => acc + val);
+}
+
+function part1(): number {
+    const crabPoses: number[] = input.join().split(/,/).map(it => Number.parseInt(it));
+    const usages: UsageMap = {};
+
+    for (let i = crabPoses.reduce((acc, it) => acc < it ? it : acc); i >= 0; i--) {
+        usages[i] = calculateFuel(i, crabPoses, (pos, crabPos) => Math.abs(pos - crabPos));
+    }
+
+    return Object.values(usages).reduce((acc, val) => acc > val ? val : acc);
+}
+
+console.log(`Part 1 : ${part1()}`);
+
+function part2(): number {
+    const crabPoses: number[] = input.join().split(/,/).map(it => Number.parseInt(it));
+    const usages: UsageMap = {};
+
+    for (let i = crabPoses.reduce((acc, it) => acc < it ? it : acc); i >= 0; i--) {
+        usages[i] = calculateFuel(i, crabPoses, (pos, crabPos) => {
+            let ret = 0;
+            for (let i = 0; i <= Math.abs(pos - crabPos); i++) {
+                ret += i;
+            }
+
+            return ret;
+        });
+    }
+
+    return Object.values(usages).reduce((acc, val) => acc > val ? val : acc);
+}
+
+console.log(`Part 1 : ${part2()}`);
